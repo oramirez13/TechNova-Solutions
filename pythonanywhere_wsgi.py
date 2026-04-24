3b504db9df229a3a7b1b6cc9cc3e0973b71b7255
ORAMI <167495273+oramirez13@users.noreply.github.com>
2026-04-24 00:08:38 -0600

    Switch database from MySQL to SQLite

--- pythonanywhere_wsgi.py
+++ pythonanywhere_wsgi.py
@@ -7,11 +7,10 @@ if PROJECT_HOME not in sys.path:
     sys.path.insert(0, PROJECT_HOME)
 
 
-os.environ["TECHNOVA_DB_ENGINE"] = "mysql"
-os.environ["TECHNOVA_DB_HOST"] = "yourusername.mysql.pythonanywhere-services.com"
-os.environ["TECHNOVA_DB_USER"] = "yourusername"
-os.environ["TECHNOVA_DB_PASSWORD"] = "your-mysql-password"
-os.environ["TECHNOVA_DB_NAME"] = "yourusername$technova"
+os.environ["TECHNOVA_DB_ENGINE"] = "sqlite"
+os.environ["TECHNOVA_SQLITE_PATH"] = (
+    "/home/yourusername/TechNova-Solutions/flask/technova_solutions_render/database/technova.sqlite3"
+)
 os.environ["SECRET_KEY"] = "replace-this-secret"
 
 
